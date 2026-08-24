b65964a43d6dc2e7ea8e1db1f24cb1da52c5df25
Ramasubbu <mramsubu@gmail.com>
2026-08-24 15:22:12 +0530

    remove docker

--- Program.cs
+++ Program.cs
@@ -2,8 +2,5 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "Hello World Test!");
-app.MapGet("/health", () => "Healthy Test!");
-app.MapGet("/sample", () => "Sample Test!");
 
 app.Run();

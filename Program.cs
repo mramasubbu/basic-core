041f8413b464854c28e6f0e9da53192312ba7b68
Ramasubbu <mramsubu@gmail.com>
2026-08-24 18:26:23 +0530

    updated API

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "It works!");
 app.MapGet("/health", () => "It is healthy!");
 
 app.Run();

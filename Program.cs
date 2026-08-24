a81900e76b3969397a392f6304db4a88336d45f9
Ramasubbu <mramsubu@gmail.com>
2026-08-24 14:40:50 +0530

    4th

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/test", () => "Hello World Test!");
-app.MapGet("/health", () => "Health Test!");
+app.MapGet("/health", () => "Healthy Test!");
 
 app.Run();
